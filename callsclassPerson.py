396797d5bf870e9467eb69e839149b48e29eab1b
JacksonJ01 <jacksonj@hartwick.edu>
2019-11-14 00:56:30 -0500

    I fudged around for too long

--- callsclassPerson.py
+++ callsclassPerson.py
@@ -2,6 +2,8 @@
 # 11/13/19
 # I will create the basis for a project I have had on my mind for a while
 from classPerson import Person
+from random import *
+
 input("PRESS ENTER")
 print('We\'re gonna make 2 people. And see what they do.')
 print("But before we do that, should we separate them, or let them socialize?")
@@ -26,28 +28,31 @@ print("Alright, now, in inches, how tall is this person?")
 height = input(">>>")
 print("And what is the weight of the person?")
 weight = input(">>>")
-
-Person1 = Person(name, age, height, weight)
+print("How strong, in numbers, is your person?")
+strength = input(">>>")
+Person1 = Person(name, age, height, weight, strength)
 
 input("PRESS ENTER TO ENTER THOSE VALUES")
 Person1.person_des()
 
-print("\nNow for the second person. What will their attributes be")
+print("\nNow for the second person. What will their attributes be.")
 print("Name:")
 name2 = input(">>>").title()
 print("Age:")
 age2 = input(">>>")
-print("Weight:")
+print("Weight in pounds:")
 weight2 = input(">>>")
-print("And the Height:")
+print("And the Height in inches:")
 height2 = input(">>>")
-
-Person2 = Person(name2, age2, height2, weight2)
+print('And the strength for this person is:')
+strength = input(">>>")
+Person2 = Person(name2, age2, height2, weight2, strength)
 
 print(f"\nSo we have {Person1.name} and {Person2.name}")
 if separate == "Separate" or separate == "Se":
     print("Hm, they seem lonely. You decided to separate them. They could have started a civilation.")
-    print("At least they aren't able to fight each other. That's good")
+    print("At least they aren't able to fight each other. That's good.")
+    exit()
 
 elif separate == "Socialize" or separate == "So":
     print("Uh- oh, I think they're gonna fight!")
@@ -60,13 +65,69 @@ elif separate == "Socialize" or separate == "So":
         exit()
     elif Person1.weight and Person1.height < Person2.weight and Person2.height:
         print("Oh my gosh! They're throwing hands!")
-        print(Person2.name, "is killing", Person1.name)
+        print(Person2.name, "is killing", Person1.name + ".")
         print(Person1.name, "is dead.")
+        print("This murder is on your hands!")
+        exit()
     else:
-        print("I think they got along..")
+        print("Wait.")
+        print("I think they're getting along...")
         print("Hey, look at that.")
         if Person1.age < Person2.age:
-            print()
+            print("Awe,", Person2.name, "is taking care of", Person1)
+            print("Good choice")
+            exit()
         elif Person1.age > Person2.age:
-            print()
+            print("Hey would you look at that. Looks like they're playing cards.")
+            print("They're friends now.")
+            print("Good job")
+            exit()
 
+        elif Person1.age == Person2.age:
+            rn = randint(-100, 100)
+            if rn < 0:
+                Warrior = Person("Warrior", 25, 72, 200, 100)
+                print("Woah! Someone is coming!!")
+                print('*Warrior has entered the chat*')
+                print("Warrior: I'm gonna fight you!")
+                if Warrior.strength > Person1.strength + Person2.strength and Warrior.weight > Person1.weight + Person2.weight and Warrior.height > Person1.height + Person2.height and Warrior.age > Person1.age + Person2.age:
+                    print("And kill you!!")
+                    print("\n\n\n\n\n")
+                    print("           88                               88")
+                    print("           88                         ,d    88")
+                    print("           88                         88    88")
+                    print(""" ,adPPYb,88  ,adPPYba, ,adPPYYba, MM88MMM 88,dPPYba,""")
+                    print("""a8"    `Y88 a8P_____88 ""     `Y8   88    88P'    "8a""")
+                    print("""8b       88 8PP""""""" ,adPPPPP88   88    88       88""")
+                    print(""" 8a,   ,d88 "8b,   ,aa 88,    ,88   88,   88       88""")
+                    print(""" `"8bbdP"Y8  `"Ybbd8"' `"8bbdP"Y8   "Y888 88       88""")
+                    exit()
+
+                elif Warrior.strength < Person1.strength + Person2.strength and Warrior.weight < Person1.weight + Person2.weight and Warrior.height < Person1.height + Person2.height and Warrior.age < Person1.age + Person2.age:
+                    print("You stabbed the warrior... the warrior named ", Warrior.name)
+                    print("You WiN")
+                    exit()
+
+            elif rn > 0:
+                Warrior = Person("Warrior", 40, 84, 300, 300)
+                print("Woah! Someone is coming!!")
+                print("*Warrior has entered the chat*")
+                print("Warrior: My name is " + Warrior.name)
+                print("         It makes no sense for you to try to fight me, but I will give a chance; 1/100th chance")
+                input("PRESS ENTER TO DECIDE THEIR FATE")
+                chance = randint(1, 100)
+                if chance == 1:
+                    print("Warrior: You actually defeated me...")
+                    print("         You have survived, but not next time!")
+                    print("You WiN")
+                    exit()
+                else:
+                    print("           88                               88")
+                    print("           88                         ,d    88")
+                    print("           88                         88    88")
+                    print(""" ,adPPYb,88  ,adPPYba, ,adPPYYba, MM88MMM 88,dPPYba,""")
+                    print("""a8"    `Y88 a8P_____88 ""     `Y8   88    88P'    "8a""")
+                    print("""8b       88 8PP""""""" ,adPPPPP88   88    88       88""")
+                    print(""" 8a,   ,d88 "8b,   ,aa 88,    ,88   88,   88       88""")
+                    print(""" `"8bbdP"Y8  `"Ybbd8"' `"8bbdP"Y8   "Y888 88       88""")
+                    exit()
--- classPerson.py
+++ classPerson.py
@@ -4,30 +4,15 @@
 
 
 class Person:
-    def __init__(self, name, age, height, weight):
+    def __init__(self, name, age, height, weight, strength):
         self.name = name
         self.age = int(age)
         self.height = int(height)
         self.weight = int(weight)
+        self.strength = int(strength)
 
     def person_des(self):
         print("Okay, we have", self.name)
         print(f"Hey, they're pretty young! Only {self.age} years old")
         print("Heh, and that's pretty short too,", self.height, "inches short")
         print("And would you look at that. A fat boi;", self.weight, "pounds heavy")
-
-
-class Warrior(Person):
-
-    def __init__(self, name, age, height, weight, strength):
-        super().__init__(name, age, height, weight)
-        self.strength = strength
-
-
-name3 = input(">>>")
-age3 = input(">>>")
-height3 = input(">>>")
-weight3 = input(">>>")
-
-bob = Warrior(name3, age3, height3, weight3)
-print(Warrior)
